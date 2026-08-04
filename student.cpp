da45e166e5174c783afad1f3dab6cb1c8526fad9
akshaymagdum8877 <akshaymagdum.asm.8877@gmail.com>
2026-08-04 04:08:14 +0000

    student

---
+++ student.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class student{
+    private:
+    string name;
+    int rollno;
+    float marks;
+
+    public:
+    void inputdetails()
+    {
+        cout<<"enter student name:";
+        getline(cin>>ws,name);
+        cout<<"enter roll number:";
+        cin>>rollno;
+        cout<<"enter marks:";
+        cin>>marks;
+
+    }
+    void displaydetails()
+    const{
+        cout<<"\n----student details----\n";
+        cout<<"name:"<<name<<endl;
+        cout<<"roll no.:;"<<rollno<<endl;
+        cout<<"marks:"<<marks<<endl;
+    }
+};
+int main()
+{
+    student s;
+    s.inputdetails();
+    s.displaydetails();
+    return 0;
+
+}
